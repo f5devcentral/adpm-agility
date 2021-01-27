7d79c1155dcc00eef28b7ae1c069716261b4b192
David Garrison <d.garrison@f5.com>
2021-01-27 12:41:51 -0800

    updated workflow

---
+++ terraform/github_actions/outputs.tf
@@ -0,0 +1,19 @@
+data "terraform_remote_state" "foo" {
+  backend = "consul"
+  config = {
+    address = "54.162.126.20:8500"
+    path = "adpm/labs/agility/students/7fb4/terraform/tfstate"
+  }
+}
+
+output "version" {
+  value = "${data.terraform_remote_state.foo.version}"
+}
+
+# output "vpc" {
+#   value = "${data.terraform_remote_state.foo.outputs.vpc_id}"
+# }
+
+# output "web_count" {
+#   value = "${data.terraform_remote_state.foo.outputs.web_server_count}"
+# }
