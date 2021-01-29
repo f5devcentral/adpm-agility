8f98d44d796c2146beb80b31912cd6bf972c581f
gregcoward <g.coward@f5.com>
2021-01-29 15:03:01 -0800

    update

---
+++ terraform-automation/autotools.tf
@@ -0,0 +1,48 @@
+# Setup Onboarding scripts
+data "template_file" "init_file" {
+  template = file("${path.module}/onboard.tpl")
+
+  vars = {
+    admin_username = var.f5_username
+    admin_password = local.upass
+    DO_URL         = var.doPackageUrl
+    AS3_URL        = var.as3PackageUrl
+    TS_URL         = var.tsPackageUrl
+    libs_dir       = var.libs_dir
+    onboard_log    = var.onboard_log
+    DO_Document    = data.template_file.vm01_do_json.rendered
+    AS3_Document   = data.template_file.as3_json.rendered
+    TS_Document    = data.template_file.ts_json.rendered
+  }
+}
+
+data "template_file" "vm01_do_json" {
+  template = file("${path.module}/do.json")
+
+  vars = {
+    hostname        = local.hostname
+    local_selfip       = "-external-self-address-"
+    gateway            = var.ext_gw
+    dns_server         = var.dns_server
+    ntp_server         = var.ntp_server
+    timezone           = var.timezone
+  }
+}
+
+data "template_file" "as3_json" {
+  template = file("${path.module}/as3.json")
+  vars = {
+    backendvm_ip    = var.backend01ext
+    web_pool        = "myapp-${var.app}"
+  }
+}
+
+  data "template_file" "ts_json" {
+  template = file("${path.module}/ts.json")
+
+  vars = {
+    region      = data.azurerm_resource_group.bigiprg.location
+    splunkIP  = var.splunkIP
+    splunkPort = var.splunkPort
+  }
+}
---
+++ terraform-automation/outputs.tf
@@ -0,0 +1,53 @@
+output mgmtPublicIP {
+  description = "The actual ip address allocated for the resource."
+  value       = data.azurerm_public_ip.f5vm01mgmtpip.ip_address
+}
+
+output mgmtPublicDNS {
+  description = "fqdn to connect to the first vm provisioned."
+  value       = data.azurerm_public_ip.f5vm01mgmtpip.fqdn
+}
+
+
+output mgmtPort {
+  description = "Mgmt Port"
+  value       = local.total_nics > 1 ? "443" : "8443"
+}
+
+output f5_username {
+  value = var.f5_username
+}
+
+output bigip_password {
+  value       = local.upass 
+}
+
+output onboard_do {
+   value      = data.template_file.clustermemberDO2[0].rendered 
+  depends_on = [data.template_file.clustermemberDO2[0]]
+
+}
+
+output "public_addresses" {
+  description = "List of BIG-IP public addresses"
+  value   = concat(azurerm_public_ip.secondary_external_public_ip.*.ip_address)
+}
+
+output "alb_address" {
+  description = "Public endpoint for load balancing external app"
+  value       = azurerm_public_ip.alb_public_ip.ip_address
+}
+
+output "external_nics" {
+  description = "List of BIG-IP external nics"
+  value   = concat(azurerm_network_interface.external_nic.*.id)
+}
+
+output "private_addresses" {
+  description = "List of BIG-IP private addresses"
+  value   = concat(azurerm_network_interface.external_nic.*.private_ip_addresses, azurerm_network_interface.external_public_nic.*.private_ip_addresses, azurerm_network_interface.internal_nic.*.private_ip_address)
+}
+
+output mgmt_nic {
+  value = azurerm_network_interface.mgmt_nic.*.id
+}
