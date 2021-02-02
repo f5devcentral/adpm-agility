1e7c3bfc2bedeb109561b88d97e337f300a86db3
gregcoward <g.coward@f5.com>
2021-02-02 11:26:18 -0800

    update ssh variable

--- terraform-automation/variables.tf
+++ terraform-automation/variables.tf
@@ -97,7 +97,7 @@ variable enable_accelerated_networking {
 variable enable_ssh_key {
   type        = bool
   description = "(Optional) Enable ssh key authentication in Linux virtual Machine"
-  default     = false
+  default     = true
 }
 
 variable script_name {
