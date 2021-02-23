c91a3118f8bd268d92c78d56a76343d3c8872233
David Garrison <d.garrison@f5.com>
2021-02-22 17:15:55 -0800

    updated workflow

---
+++ workflow_automation/f5module/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~> 0.13"
+  required_providers {
+       azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~>2.28.0"
+         }   
+       random = {
+         source = "hashicorp/random"
+         version = "~>2.3.0"
+       }
+       template = {
+         source = "hashicorp/template"
+         version = "~>2.1.2"
+       }
+       null = {
+         source = "hashicorp/null"
+         version = "~>2.1.2"
+      }
+   }
+ } 
